789dbd1b5062c1e7ca676978fad0feefad2b24e3
Daehyuk Ahn <daehyuk@live.com>
2021-05-21 22:46:16 -0700

    Bugfix 8

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -106,9 +106,9 @@ void Sidebar::paintEvent(QPaintEvent *event) {
   p.drawImage(58, 196, signal_imgs[strength]);
   configFont(p, "Open Sans", 35, "Regular");
   p.setPen(QColor(0xff, 0xff, 0xff));
-  const QRect r = QRect(50, 247, 100, 50);
+  const QRect r = QRect(50, 237, 100, 50);
   p.drawText(r, Qt::AlignCenter, network_type[net_type]);
-  p.drawText(0, r.top() + 30, event->rect().width(), 50, Qt::AlignCenter, network_address);
+  p.drawText(0, r.top() + 40, event->rect().width(), 50, Qt::AlignCenter, network_address);
 
   // battery percent, image
   if (battery_percent <= 1) return;
